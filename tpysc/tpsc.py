245c67dc922135fb575061fba109d6a1b20e31f7
Jerome <LJerome94@github.com>
2025-04-18 09:46:16 -0400

    refactor(tpsc.py): Change functions naming conventions to PEP8

--- tpysc/tpsc.py
+++ tpysc/tpsc.py
@@ -60,7 +60,7 @@ class TPSC:
         self.traceSG2 = None
 
 
-    def calcFirstLevelApprox(self):
+    def calc_first_level_approx(self):
         """
         Do the first level of approximation of TPSC.
         This calculates chi1, and then obtains chisp and chich from the sum rules and the TPSC ansatz.
@@ -68,20 +68,20 @@ class TPSC:
         :meta private:
         """
         # Calculate chi1
-        self.calcChi1()
+        self.calc_chi1()
 
         # Calculate Usp and Uch from the TPSC ansatz
-        self.calcUsp()
-        self.calcUch()
+        self.calc_usp()
+        self.calc_uch()
 
         # Calculate the double occupancy
-        self.docc = self.calcDoubleOccupancy()
+        self.docc = self.calc_double_occupancy()
 
         # Calculate the spin correlation length
-        self.calcXispCommensurate()
+        self.calc_xisp_commensurate()
         return
 
-    def calcChi1(self):
+    def calc_chi1(self):
         """
         Function to calculate chi1(q,iqn).
         This also calculates the trace of chi1(q,iqn) as a consistency check.
@@ -105,7 +105,7 @@ class TPSC:
         self.traceChi1 = self.mesh.trace(self.chi1, 'B')
 
 
-    def calcUsp(self):
+    def calc_usp(self):
         """
         Function to compute Usp from chi1 and the sum rule.
 
@@ -116,21 +116,21 @@ class TPSC:
         Uspmax = 2./np.amax(self.chi1).real-1e-7 # Note: the 1e-7 is chosen for stability purposes
 
         # Calculate Usp
-        self.Usp = brentq(lambda m: self.calcSumChisp(m)-self.calcSumRuleChisp(m), Uspmin, Uspmax, disp=True)
+        self.Usp = brentq(lambda m: self.calc_sum_chisp(m)-self.calc_sum_rule_chisp(m), Uspmin, Uspmax, disp=True)
 
 
-    def calcUch(self, Uchmin=0., Uchmax=100.):
+    def calc_uch(self, Uchmin=0., Uchmax=100.):
         """
         Function to compute Uch from chi1 and the sum rule.
-        Note: calcUsp has to be called before this function.
+        Note: calc_usp has to be called before this function.
 
         :meta private:
         """
         # Calculate Uch
-        self.Uch = brentq(lambda m: self.calcSumChich(m)-self.calcSumRuleChich(self.Usp), Uchmin, Uchmax, disp=True)
+        self.Uch = brentq(lambda m: self.calc_sum_chich(m)-self.calc_sum_rule_chich(self.Usp), Uchmin, Uchmax, disp=True)
 
 
-    def calcSumChisp(self, Usp):
+    def calc_sum_chisp(self, Usp):
         """
         Function to compute the trace of chisp(q) = chi1(q)/(1-Usp/2*chi1(q)).
         Also sets chisp(q,iqn) and finds the maximal value.
@@ -142,7 +142,7 @@ class TPSC:
         return self.mesh.trace(self.chisp, 'B').real
 
 
-    def calcSumChich(self, Uch):
+    def calc_sum_chich(self, Uch):
         """
         Function to compute the trace of chich(1) = chi1(q)/(1+Uch/2*chi1(q)).
         Also sets chich(q,iqn).
@@ -153,10 +153,10 @@ class TPSC:
         return self.mesh.trace(self.chich, 'B').real
 
 
-    def calcDoubleOccupancy(self):
+    def calc_double_occupancy(self):
         """
         Function to compute the double occupancy.
-        Note: the function calcUsp has to be called before this one
+        Note: the function calc_usp has to be called before this one
         The TPSC ansatz we use here satisfies the particle-hole symmetry with:
         n<1: Usp = U<n_up n_dn>/(<n_up><n_dn>)
         n>1: Usp = U<(1-n_up)(1-n_dn)>/(<(1-n_up)><(1-n_dn)>)
@@ -169,7 +169,7 @@ class TPSC:
             return self.Usp/(4*self.U)*(2-self.n)*(2-self.n)-1+self.n
 
 
-    def calcSumRuleChisp(self, Usp):
+    def calc_sum_rule_chisp(self, Usp):
         """
         Calculate the spin susceptibility sum rule for a specific Usp and U.
         The TPSC ansatz we use here satisfies the particle-hole symmetry with:
@@ -184,7 +184,7 @@ class TPSC:
             return self.n - Usp/(2*self.U)*(2-self.n)*(2-self.n)+2-2*self.n
 
 
-    def calcSumRuleChich(self, Usp):
+    def calc_sum_rule_chich(self, Usp):
         """
         Calculate the charge susceptibility sum rule for a specific Usp and U.
         The TPSC ansatz we use here satisfies the particle-hole symmetry with:
@@ -199,7 +199,7 @@ class TPSC:
             return self.n + Usp/(2*self.U)*(2-self.n)*(2-self.n)-2+2*self.n - self.n*self.n
 
 
-    def calcXispCommensurate(self):
+    def calc_xisp_commensurate(self):
         """
         Compute the spin correlation length from commensurate spin fluctuations at Q=(pi,pi).
         This calculates the width at half maximum of the spin susceptibility ONLY if its maximal value is at (pi,pi).
@@ -229,10 +229,10 @@ class TPSC:
             self.xisp = 1/(np.pi - qHM - q0)
 
 
-    def calcSecondLevelApprox(self):
+    def calc_second_level_approx(self):
         """
         Function to calculate the self-energy in the second level of approximation of TPSC.
-        Important: The function calcFirstLevelApprox must be called before this one.
+        Important: The function calc_first_level_approx must be called before this one.
         Note: The Hartree term (Un/2) is not included here.
         The TPSC self-energy is: U/8 sum_q(3chi_sp(q)U_sp + chi_ch(q)U_ch)G1(k+q).
         We define V(q) =  U/8(3chi_sp(q)U_sp + chi_ch(q)U_ch) and compute 1/2(V(r)*G(-r)+V(-r)G(r)).
@@ -263,7 +263,7 @@ class TPSC:
         return
 
 
-    def checkSelfConsistency(self):
+    def check_self_consistency(self):
         """
         Function to check the self-consistency between one- and two-particle quantities through:
         Tr[Self-Energy*Green's function] = U<n_up n_dn> - Un^2/4
@@ -282,7 +282,7 @@ class TPSC:
         self.exactTraceSelfG = self.U*self.docc-self.U*self.n*self.n/4
 
 
-    def run(self):
+    def solve(self):
         """
         Run the TPSC method
 
@@ -290,9 +290,9 @@ class TPSC:
         :rtype: dict
         """
         # Make the calculation
-        self.calcFirstLevelApprox()
-        self.calcSecondLevelApprox()
-        self.checkSelfConsistency()
+        self.calc_first_level_approx()
+        self.calc_second_level_approx()
+        self.check_self_consistency()
 
         # Prepare output
         self.main_results = {
@@ -308,6 +308,7 @@ class TPSC:
         }
         return self.main_results
 
+
     def printResults(self):
         """
         Print results to screen
@@ -318,6 +319,7 @@ class TPSC:
         for key,value in self.main_results.items():
             print(f"{key}: {value:5e}")
         return
+
 
     def writeResultsJSON(self, filename):
         """
@@ -343,6 +345,7 @@ class TPSC:
         with open(filename, 'w') as outfile:
            outfile.write(json.dumps(out_results, indent=4))
         return
+
 
     def plotSelfEnergyVsWn(self, coordinates, Wn_range, show=True, ax=None):
         """
